dd877f53cec82e69302bdac2632c2be69ccb5203
Alikhan Batchaev <loko-han@mail.ru>
2020-03-14 17:19:41 +0300

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by 🧔🏻Alikhan Batchaev on 14.03.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
